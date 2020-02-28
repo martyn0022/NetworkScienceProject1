1731af87e73089fa3d3ff2615ccc2fac9f558457
Seannyyy <43570494+Seannyyy@users.noreply.github.com>
2020-02-28 19:37:00 +0800

    Update Network to add edges to graph

--- NetworkGraph.py
+++ NetworkGraph.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 
 
 class Network:
-    def __init__(self, conf):
-        print("initializing networkgraph")
+    def __init__ (self, conf):
         self.conferenceDetails = conf
         # conferences are the nodes
         self.graph = nx.Graph()
 
-    def DrawGraph(self):
+    def DrawGraph (self):
         self.AddNodesToNetwork()
         nx.draw(self.graph)
         plt.savefig("test.png")
@@ -18,10 +17,34 @@ class Network:
     # add range to filter
     def AddNodesToNetwork (self):
         nodes = self.FilterNodes(2000)
+        edges = self.addEdgesToNetwork(nodes)
         self.graph.add_nodes_from(nodes)
+        self.graph.add_edges_from(edges)
 
-    def addEdgesToNetwork(self):
-        return
+    def addEdgesToNetwork (self, nodes):
+        edges = []
+        for key1 in nodes:
+            for key2 in nodes:
+                weight = 0
+                if key1 != key2:
+                    for author1 in self.conferenceDetails[key1]['authors']:
+                        if author1 in self.conferenceDetails[key2]['authors']:
+                            weight += 1
+                    if weight > 0:
+                        edges.append((key1, key2, {'weight': weight}))
+
+        '''
+        for key1, conference1 in self.conferenceDetails.items():
+            for key2, conference2 in self.conferenceDetails.items():
+                weight = 0
+                if key1 != key2:
+                    for author1 in conference1['authors']:
+                        if author1 in conference2['authors']:
+                            weight += 1
+                    if weight > 0:
+                        edges.append((key1, key2, {'weight': weight}))
+        '''
+        return edges
 
     def FilterNodes (self, yearfilter):
         conferenceNodes = []
