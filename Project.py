907d35da6e9b2d9b254b3d3e39f350021dc904ed
martyn <32283437+martyn0022@users.noreply.github.com>
2021-04-10 13:53:37 +0800

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,13 +1,13 @@
 import Preprocessing as ps
-import Science as sc
-import Interface
+import Faculty as sc
+import gui
 import json
 
-conferenceAuthorJSON = "json/conferencesAndAuthors.json"
-authorJSON = "json/authors.json"
-inproceedsJSON = 'json/inproceeds.json'
-dblpFilename = "dblp.xml"
-listOfJSON = [conferenceAuthorJSON, authorJSON, inproceedsJSON]
+conferenceAuthorJSON = "json/ScseStaff.json"
+#authorJSON = "json/authors.json"
+#inproceedsJSON = 'json/inproceeds.json'
+#dblpFilename = "dblp.xml"
+#listOfJSON = [conferenceAuthorJSON, authorJSON, inproceedsJSON]
 
 
 def main ():
